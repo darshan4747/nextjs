f6663c5b749148486b41ba21ece0aad259264994
Darshan Bargal <56996649+darshan4747@users.noreply.github.com>
2024-04-11 15:44:05 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
+        <p>Here is the answer <a href="https://docs.google.com/document/d/168lVbQsMkMhqm9BHANBgOOm211HL1OZX_6NwpRP5ol0/edit">Link to the Document</a></p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
